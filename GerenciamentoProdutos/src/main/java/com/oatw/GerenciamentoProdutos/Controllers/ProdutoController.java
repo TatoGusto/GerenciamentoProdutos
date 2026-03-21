0c998174c3f8f64a16d584974a94b57d5f51bbc5
OtavioAugusto <santostatogusto@gmail.com>
2026-03-21 19:53:16 -0300

    Controller criado

---
+++ GerenciamentoProdutos/src/main/java/com/oatw/GerenciamentoProdutos/Controllers/ProdutoController.java
@@ -0,0 +1,44 @@
+package com.oatw.GerenciamentoProdutos.Controllers;
+
+import com.oatw.GerenciamentoProdutos.Models.ProdutoModel;
+import com.oatw.GerenciamentoProdutos.Services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/produtos")
+public class ProdutoController {
+
+    @Autowired
+    ProdutoService produtoService;
+
+    @PostMapping
+    public ResponseEntity<ProdutoModel> createProduto(ProdutoModel produtoModel){
+        ProdutoModel request = produtoService.createProduto(produtoModel);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(produtoModel.getId()).toUri();
+        return ResponseEntity.created(uri).body(request);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoModel>> getAll(){
+        List<ProdutoModel> request = produtoService.getAll();
+        return ResponseEntity.ok().body(request);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoModel> getById(@PathVariable Long id){
+        ProdutoModel request = produtoService.getById(id);
+        return ResponseEntity.ok().body(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable Long id){
+        produtoService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
